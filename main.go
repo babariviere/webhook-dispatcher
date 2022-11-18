c05742a27951a51294eafc2531f17b8c50eaba96
Bastien Riviere <me@babariviere.com>
2022-11-18 19:59:58 +0100

    feat: add dispatcher functionality

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -9,52 +11,78 @@ import (
 )
 
 type Location struct {
-    // Where to dispatch the request
-    URL string
+	// Where to dispatch the request
+	URL string
 }
 
 type HttpConfig struct {
-    Address string
-    Port int
+	Address string
+	Port    int
 }
 
 type Config struct {
-    Locations map[string]Location
-    Http HttpConfig
+	Locations map[string][]Location
+	Http      HttpConfig
 }
 
 func dispatch(cfg Config) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
+	client := http.Client{}
+	return func(w http.ResponseWriter, r *http.Request) {
+		locations, ok := cfg.Locations[r.URL.Path]
+		if !ok {
+			log.Printf("Unconfigured URL path got called: %s", r.URL.Path)
+			http.NotFound(w, r)
+			return
+		}
 
-    }
+		var bodyBytes []byte
+		if r.Body != nil {
+			bodyBytes, _ = ioutil.ReadAll(r.Body)
+		}
+
+		log.Printf("Dispatching request for path: %s", r.URL.Path)
+		for _, location := range locations {
+			req, err := http.NewRequest(r.Method, location.URL, ioutil.NopCloser(bytes.NewBuffer(bodyBytes)))
+			if err != nil {
+				log.Printf("Invalid request in config for path %s: %s", r.URL.Path, err)
+				continue
+			}
+			_, err = client.Do(req)
+			if err != nil {
+				log.Printf("Request failed: %s", err)
+			}
+		}
+
+        w.Write([]byte("OK\n"))
+	}
 }
 
 func main() {
-    cfg := LoadConfig()
+	cfg := LoadConfig()
 
-    http.Handle("/", dispatch(cfg))
+	http.Handle("/", dispatch(cfg))
 
-    log.Printf("Listening on address %s:%v", cfg.Http.Address, cfg.Http.Port)
-    log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%v", cfg.Http.Address, cfg.Http.Port), nil))
+	log.Printf("Listening on address %s:%v", cfg.Http.Address, cfg.Http.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%v", cfg.Http.Address, cfg.Http.Port), nil))
 }
 
 func LoadConfig() (config Config) {
-    viper.SetConfigName("config")
-    viper.SetConfigType("json")
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
 
-    viper.AddConfigPath(".")
-    viper.AutomaticEnv()
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
 
-    if err := viper.ReadInConfig(); err != nil {
-        log.Fatalf("Error reading config file: %s", err)
-    }
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %s", err)
+	}
 
-    viper.SetDefault("http.address", "0.0.0.0")
-    viper.SetDefault("http.port", "8080")
+	viper.SetDefault("http.address", "0.0.0.0")
+	viper.SetDefault("http.port", "8080")
 
-    if err := viper.Unmarshal(&config); err != nil {
-        log.Fatalf("Unable to decode configuration file: %s", err)
-    }
+	if err := viper.Unmarshal(&config); err != nil {
+		log.Fatalf("Unable to decode configuration file: %s", err)
+	}
 
-    return
+	return
 }
